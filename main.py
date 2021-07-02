7eb9099b617c8b326a2b8d7745a9bc66fcaff68b
Alex <adriensage.alexandre@gmail.com>
2021-07-02 13:56:35 +0200

    Ajout du ficher main en python

--- main.py
+++ main.py
@@ -1,50 +1,49 @@
-# Premier projet en python que j'ai fait il y a un an
-# Test avec Github
-
 print("\nBienvenue dans l'outil servant à résoudre des équations par dichotomie !")
 print("\nL'équation est une équation de type : ax + b")
 a = int(input("Rentrez ce qui correspond à 'A' dans votre équation : "))
 b = int(input("Rentrez ce qui correspond à 'B' dans votre équation : "))
-print("Votre équation est donc : ", a,"x + ", b)
+print(f"Votre équation est donc : {a}x + {b}\n")
 t = 0
 temps = 0
 
 A1 = -100
 A2 = 100
-A3 = a * 0 + b
+A3 = b
 
 image = a * A1 + b
 image2 = a * A2 + b
 
-print("")
+if b == 0:
+    print("La solution de l'équation est 0")
+    print(f"Il m'a fallut {temps} boucles pour trouver ce résultat")
+else:
+    for i in range(30):
+        temps += 1
+        image = a * A1 + b
+        image2 = a * A2 + b
 
-while temps < 30:
-    temps = temps + 1
-    image = a * A1 + b
-    image2 = a * A2 + b
+        if abs(image) - abs(image2) < 0:
+            # Il faut aller à gauche
+            A3 = (A1 + A2) / 2
+            A2 = A3
+            A3 = (A1 + A2) / 2
 
-    if abs(image) - abs(image2) < 0:
-        #print("Il faut aller à gauche")
-        A3 = (A1 + A2) / 2
-        A2 = A3
-        A3 = (A1 + A2) / 2
+        elif abs(image) - abs(image2) > 0:
+            # Il faut aller à droite
+            A3 = (A1 + A2) / 2
+            A1 = A3
+            A3 = (A1 + A2) / 2
 
-    elif abs(image) - abs(image2) > 0:
-        #print("Il faut aller à droite")
-        A3 = (A1 + A2) / 2
-        A1 = A3
-        A3 = (A1 + A2) / 2
+        else:
+            print("haaaaaaaaaaaaaaaaaaa")
 
-    else:
-        print("haaaaaaaaaaaaaaaaaaa")
-        
-    s = [abs(A1),abs(A2)]
+        s = [abs(A1),abs(A2)]
 
-    if max(s) - min(s) < 0.001:
-        print("Votre solution est entre ces deux nombres : ")
-        print(round(A1, 5))
-        print(round(A2, 5))
-        print("Il m'a fallut", temps, "boucles pour trouver ce résultat")
-        break
-    else :
-        continue
+        if max(s) - min(s) < 0.001:
+            print("Votre solution est entre ces deux nombres : ")
+            print(round(A1, 5))
+            print(round(A2, 5))
+            print(f"Il m'a fallut {temps} boucles pour trouver ce résultat")
+            break
+        else :
+            continue
